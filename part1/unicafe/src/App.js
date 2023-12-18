af74ecaec942f2ccbb897187f9f7eaaa7a7c4ad2
CTOPUS O <767420090@qq.com>
2023-12-18 15:41:29 +0200

    refactor: Statistics calling style

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,43 +1,39 @@
 import { useState } from 'react'
-
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr><td>{text}</td><td>{value}</td></tr>
+  )
+}
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={all / 3} />
+        <StatisticLine text="positive" value={(good / all) * 100 + "%"} />
+      </tbody>
+    </table>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-
-  const Button = (props) => {
-    return (
-      <button onClick={props.onClick}>
-        {props.text}
-      </button>
-    )
-  }
-  const StatisticLine = ({ text, value }) => {
-    return (
-      <tr><td>{text}</td><td>{value}</td></tr>
-    )
-  }
-  const Statistics = (props) => {
-    const all = good + neutral + bad
-    if (all === 0) {
-      return <p>No feedback given</p>
-    }
-    return (
-      <table>
-        <tbody>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={all} />
-          <StatisticLine text="average" value={all / 3} />
-          <StatisticLine text="positive" value={(good / all) * 100 + "%"} />
-        </tbody>
-      </table>
-    )
-  }
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -45,7 +41,7 @@ const App = () => {
       <Button onClick={() => { setNeutral(neutral + 1) }} text="neutral" />
       <Button onClick={() => { setBad(bad + 1) }} text="bad" />
       <h1>Statistic</h1>
-      {Statistics()}
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
