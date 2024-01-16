37fcd7cc76f966a7af7104dbfe5598b079d195ff
CTOPUS O <767420090@qq.com>
2024-01-16 15:11:57 +0200

    func: 7.1 - 7.3 Route in the frontend

---
+++ part7/routed-anecdotes/src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, Route, useNavigate
+} from 'react-router-dom'
+import Anecdote from './components/Anecdote'
+import AnecdoteList from './components/AnecdoteList'
+import Menu from './components/Menu'
+import About from './components/About'
+import Footer from './components/Footer'
+import CreateNew from './components/CreateNew'
+import Notification from './components/Notification'
+
+
+const App = () => {
+  const navigate = useNavigate()
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  const [notification, setNotification] = useState('')
+
+  // 7.3 automatically to showing the view for all anecdotes and the user
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    setAnecdotes(anecdotes.concat(anecdote))
+    navigate('/')
+    setNotification(`a new anecdote ${anecdote.content} created!`);
+
+    setTimeout(() => { setNotification('') }, 5000);
+  }
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  }
+
+  // 7.1 Add React Router to the application 
+  return (
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <Notification notification={notification} />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/create" element={<CreateNew addNew={addNew} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/anecdotes/:id" element={<Anecdote anecdotes={anecdotes} />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ part7/routed-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+// 7.2 Implement a view for showing a single anecdote
+const AnecdoteList = ({ anecdotes }) => {
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            <ul>
+                {anecdotes.map(anecdote => <li key={anecdote.id} >
+                    <Link to={`/anecdotes/${anecdote.id}`}> {anecdote.content} </Link> </li>)}
+            </ul>
+        </div>
+    )
+}
+
+
+export default AnecdoteList
